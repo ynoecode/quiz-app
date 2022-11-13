f96d3554e95b571720581234446e3258987ea79a
Francis Ignacio <ynoecode@gmail.com>
2022-11-13 19:53:00 +0800

    refactor: for GitHub Page

---
+++ Questions.js
@@ -0,0 +1,409 @@
+const data = 
+[
+  {
+    "category": "Science",
+    "id": "622a1c377cc59eab6f95047f",
+    "correctAnswer": "a synonym for taxonomy",
+    "incorrectAnswers": [
+      "the phylum Bryozoa",
+      "interactions among organisms and the water cycle",
+      "a branch of theology concerned with the final events in the history of the world or of mankind"
+    ],
+    "question": "What is Taxology the study of?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3e7cc59eab6f9522b0",
+    "correctAnswer": "2",
+    "incorrectAnswers": [
+      "10",
+      "1",
+      "0"
+    ],
+    "question": "How is the binary number '10' expressed as a decimal?",
+    "tags": [
+      "science",
+      "numbers",
+      "mathematics",
+      "computing"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c377cc59eab6f9504ce",
+    "correctAnswer": "prehistoric people and human origins",
+    "incorrectAnswers": [
+      "interactions between organisms and geomorphological processes",
+      "study of refuse and trash",
+      "crime"
+    ],
+    "question": "What is Paleoanthropology the study of?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c377cc59eab6f95052a",
+    "correctAnswer": "gross and disgusting things",
+    "incorrectAnswers": [
+      "crayfish",
+      "plant nutrition and growth in relation to soil conditions",
+      "periodic biological phenomena such as flowering, migration, breeding, etc"
+    ],
+    "question": "What is Grossology the study of?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c377cc59eab6f95051d",
+    "correctAnswer": "the history, languages, and cultures of the Indian subcontinent",
+    "incorrectAnswers": [
+      "the physiological bases of psychological processes",
+      "relationships between humans and animals over time through examination of animal remains at archaeological sites",
+      "proper names"
+    ],
+    "question": "What is Indology the study of?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3e7cc59eab6f952261",
+    "correctAnswer": "Hyper Text Transfer Protocol",
+    "incorrectAnswers": [
+      "Hard Time Telecommunications Pact",
+      "Head To Tail Photons",
+      "High Top Transduction Paths"
+    ],
+    "question": "In computer networking what do the initials HTTP stand for?",
+    "tags": [
+      "science",
+      "computing",
+      "the_internet",
+      "initials"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3a7cc59eab6f950fda",
+    "correctAnswer": "Coca Cola",
+    "incorrectAnswers": [
+      "Dr Pepper",
+      "Lucozade",
+      "Irn Bru"
+    ],
+    "question": "Which Significant Did Dr John Pemberton Of Atlanta Georgia Invent In 1886?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "6243341fcfaae40c12961424",
+    "correctAnswer": "A Billy",
+    "incorrectAnswers": [
+      "A Macho",
+      "A Drone",
+      "A Ram"
+    ],
+    "question": "A male goat is known as what?",
+    "tags": [
+      "animals",
+      "words",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c377cc59eab6f95054a",
+    "correctAnswer": "The practice of breaking free from restraints",
+    "incorrectAnswers": [
+      "Schematic layouts",
+      "Survival during natural disasters",
+      "Volcanoes"
+    ],
+    "question": "What is escapology the study of?",
+    "tags": [
+      "words",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c377cc59eab6f9504fe",
+    "correctAnswer": "mammals",
+    "incorrectAnswers": [
+      "wonders, or monsters",
+      "wine and winemaking",
+      "mechanical, physical, and biochemical functions of living organisms"
+    ],
+    "question": "What is Mammalogy the study of?",
+    "tags": [
+      "words",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "624437b1746187c5e7be936c",
+    "correctAnswer": "It is used in disinfectants and antiseptics.",
+    "incorrectAnswers": [
+      "It is used in nuclear reactors, as a neutron moderator.",
+      "It is used as oscillators in radios, watches.",
+      "It is used for surface purification, bleaching, odour removal and water disinfection."
+    ],
+    "question": "What is the chemical isopropyl alcohol often used for?",
+    "tags": [
+      "chemistry",
+      "general_knowledge",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3a7cc59eab6f95104f",
+    "correctAnswer": "Distance",
+    "incorrectAnswers": [
+      "Time",
+      "Pressure",
+      "Magnetism"
+    ],
+    "question": " What does a micron measure?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3a7cc59eab6f9510b7",
+    "correctAnswer": "Bones",
+    "incorrectAnswers": [
+      "Eyes",
+      "Arms",
+      "Stomach"
+    ],
+    "question": "In what body part does an osteopath specialise?",
+    "tags": [
+      "anatomy",
+      "words",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3a7cc59eab6f951107",
+    "correctAnswer": "Rattlesnakes",
+    "incorrectAnswers": [
+      "Spiders",
+      "Frogs",
+      "Beetles"
+    ],
+    "question": "__________ gather in groups to sleep through the winter. ",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "62433426cfaae40c12961427",
+    "correctAnswer": "A Gander",
+    "incorrectAnswers": [
+      "A Leopardess",
+      "A Bitch",
+      "A Hembra"
+    ],
+    "question": "What is the word for a female goose?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3a7cc59eab6f9510d1",
+    "correctAnswer": "Heart",
+    "incorrectAnswers": [
+      "Soul",
+      "Blood",
+      "Eyes"
+    ],
+    "question": "What part of the body does carditis affect?",
+    "tags": [
+      "medicine",
+      "words",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c377cc59eab6f9504ae",
+    "correctAnswer": "alternative method of massage, manipulative therapy|therapy or pressure on certain points of the sole of the feet as a means of relieving nervous tension",
+    "incorrectAnswers": [
+      "feces",
+      "study of the mouth and its diseases",
+      "karyotypes"
+    ],
+    "question": "What is Reflexology the study of?",
+    "tags": [
+      "words",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3e7cc59eab6f95228b",
+    "correctAnswer": "32",
+    "incorrectAnswers": [
+      "2",
+      "50",
+      "24"
+    ],
+    "question": "How many teeth does thr average adult human have?",
+    "tags": [
+      "biology",
+      "anatomy",
+      "numbers",
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3a7cc59eab6f951052",
+    "correctAnswer": "Buck",
+    "incorrectAnswers": [
+      "Doe",
+      "Colt",
+      "A Johnny"
+    ],
+    "question": "Which of these is a name for a male deer?",
+    "tags": [
+      "science"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  },
+  {
+    "category": "Science",
+    "id": "622a1c3a7cc59eab6f9510aa",
+    "correctAnswer": "Milky way",
+    "incorrectAnswers": [
+      "Andromeda",
+      "Ursa",
+      "Orion"
+    ],
+    "question": "What is the name of the galaxy that includes our solar system?",
+    "tags": [
+      "science",
+      "astronomy",
+      "physics",
+      "space",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [
+      
+    ]
+  }
+]
+
+export { data };
